ca1689dee627ee17a1dd17627b02d12cad2e6be1
Ram Gopal Shukla <88874177+RAM28EC@users.noreply.github.com>
2024-03-10 16:03:22 +0530

    Update main.tf

--- Assignment/root/main.tf
+++ Assignment/root/main.tf
@@ -1,1 +1,80 @@
 
+module "vpc" {
+    source = "../modules/vpc"
+    region = var.aws_region
+    project_name = var.project_name
+    vpc_cidr         = var.vpc_cidr
+    pub_sub_1a_cidr = var.pub_sub_1a_cidr
+    pub_sub_2b_cidr = var.pub_sub_2b_cidr
+    pri_sub_3a_cidr = var.pri_sub_3a_cidr
+    pri_sub_4b_cidr = var.pri_sub_4b_cidr
+    pri_sub_5a_cidr = var.pri_sub_5a_cidr
+    pri_sub_6b_cidr = var.pri_sub_6b_cidr
+}
+
+module "nat" {
+  source = "../modules/nat"
+
+  pub_sub_1a_id = module.vpc.pub_sub_1a_id
+  igw_id        = module.vpc.igw_id
+  pub_sub_2b_id = module.vpc.pub_sub_2b_id
+  vpc_id        = module.vpc.vpc_id
+  pri_sub_3a_id = module.vpc.pri_sub_3a_id
+  pri_sub_4b_id = module.vpc.pri_sub_4b_id
+  pri_sub_5a_id = module.vpc.pri_sub_5a_id
+  pri_sub_6b_id = module.vpc.pri_sub_6b_id
+}
+
+module "security-group" {
+  source = "../modules/security-group"
+  vpc_id = module.vpc.vpc_id
+  alb_sg = var.alb_sg
+}
+
+# creating Key for instances
+module "key" {
+  source = "../modules/key"
+}
+
+# Creating Application Load balancer
+module "alb" {
+  source         = "../modules/alb"
+  project_name   = module.vpc.project_name
+  alb_sg_id  =       module.security-group.alb_sg_id
+  pub_sub_1a_id = module.vpc.pub_sub_1a_id
+  pub_sub_2b_id = module.vpc.pub_sub_2b_id
+  vpc_id         = module.vpc.vpc_id
+  tg_arn = module.asg.tg_arn
+}
+
+module "asg" {
+  source         = "../modules/asg"
+  project_name   = module.vpc.project_name
+  key_name       = module.key.key_name
+  client_sg_id   = module.security-group.client_sg_id
+  pri_sub_3a_id = module.vpc.pri_sub_3a_id
+  pri_sub_4b_id = module.vpc.pri_sub_4b_id
+  tg_arn         = module.alb.tg_arn
+  s3_bucket_name = var.s3_bucket_name
+  aws_access_key_id = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+  aws_region = var.aws_region
+  
+}
+
+# creating RDS instance
+
+
+# create cloudfront distribution 
+module "cloudfront" {
+  source = "../modules/cloudfront"
+  certificate_domain_name = var.certificate_domain_name
+  alb_domain_name = module.alb.alb_dns_name
+  additional_domain_name = var.additional_domain_name
+  project_name = module.vpc.project_name
+  alb_dns_name = module.alb.alb_dns_name
+}
+
+
+# Add record in route 53 hosted zone
+
